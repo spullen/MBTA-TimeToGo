42a019fc461b5c21029b68eb70deafe52d0d33ff
Scott Pullen <s.pullen05@gmail.com>
2015-08-08 12:12:52 -0400

    Setting the timezone to be est

---
+++ src/main/groovy/net/scottpullen/config/ServiceInitialization.groovy
@@ -0,0 +1,18 @@
+package net.scottpullen.config
+
+import groovy.util.logging.Slf4j
+import org.springframework.boot.context.embedded.ServletContextInitializer
+import org.springframework.context.annotation.Configuration
+
+import javax.servlet.ServletContext
+import javax.servlet.ServletException
+
+@Slf4j
+@Configuration
+class ServiceInitialization implements ServletContextInitializer {
+
+    @Override
+    void onStartup(ServletContext servletContext) throws ServletException {
+        TimeZone.default = TimeZone.getTimeZone("EST")
+    }
+}
